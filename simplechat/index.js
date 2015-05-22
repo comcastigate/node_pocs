f008dd1bb8aeee8ca4db2293329bc1c173873c5c
Nanthakumar G <nanthakumar.ganesan@igate.com>
2015-05-22 18:44:48 +0530

    Designed application page

--- simplechat/index.js
+++ simplechat/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var bunyan = require('bunyan');
+var path = require('path');
 //var process = require('process');
 var app = express();
-var port = 3700;
+var port = 3000;
  
 /*app.get("/", function(req, res){
     res.send("It works!");
@@ -11,13 +12,22 @@ app.use(express.static(__dirname + '/public'));
 
 var io = require('socket.io').listen(app.listen(port));
 var log = bunyan.createLogger({name: process.env.COMPUTERNAME});
+
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', { message: 'Welcome to the chat' });
     socket.on('send', function (data) {
 	    console.log(process.env.COMPUTERNAME);
 		log.info({sender:'Sender Name', message: data},"Sender");
 		log.warn({foo: 'bar'}, 'hi');
 		log.error({foo: 'error'}, 'error');
+
+		/*var log_details = bunyan.createLogger({
+			name: 'Chat-Logs',
+			streams: [{
+				path: path.join(__dirname, '/log/foo.log')
+			}]
+		});*/
+		
         io.sockets.emit('message', data);
     });
 });
